864d90f982bedd6f6f8294185c370ab35f167dfe
LucasFlaquer <lucas.flaquer@gmail.com>
2021-04-15 22:53:49 -0300

    created homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,43 @@
-import { GetStaticProps } from 'next';
+// import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { PostList } from '../components/Posts/PostList';
 
-import { getPrismicClient } from '../services/prismic';
+// import { getPrismicClient } from '../services/prismic';
 
-import commonStyles from '../styles/common.module.scss';
-import styles from './home.module.scss';
+// import commonStyles from '../styles/common.module.scss';
+// import styles from './home.module.scss';
 
-interface Post {
-  uid?: string;
-  first_publication_date: string | null;
-  data: {
-    title: string;
-    subtitle: string;
-    author: string;
-  };
-}
-
-interface PostPagination {
-  next_page: string;
-  results: Post[];
-}
-
-interface HomeProps {
-  postsPagination: PostPagination;
-}
-
-// export default function Home() {
-//   // TODO
+// interface Post {
+//   uid?: string;
+//   first_publication_date: string | null;
+//   data: {
+//     title: string;
+//     subtitle: string;
+//     author: string;
+//   };
 // }
+
+// interface PostPagination {
+//   next_page: string;
+//   results: Post[];
+// }
+
+// interface HomeProps {
+//   postsPagination: PostPagination;
+// }
+
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>spacetraveling</title>
+      </Head>
+      <main>
+        <PostList />
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
