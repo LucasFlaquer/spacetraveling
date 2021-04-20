e1614a350fcdb1f96375f333a929fd5fe22ccbd1
LucasFlaquer <lucas.flaquer@gmail.com>
2021-04-20 08:32:35 -0300

    header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import styles from './header.module.scss';
 export default function Header(): JSX.Element {
   return (
     <header className={styles.Header}>
-      <img src="images/Logo.svg" alt="logo" />
+      <img src="/images/Logo.svg" alt="logo" />
     </header>
   );
 }
